1652489efc881c5025852f4a70c8ec073ca7da21
David <davidyuen0305@gmail.com>
2019-09-17 16:07:29 -0700

    removed the module folders from repository.

--- bamazon.sql
+++ bamazon.sql
@@ -7,10 +7,9 @@ USE bamazon_db;
 
 CREATE TABLE products (
     id INT NOT NULL AUTO_INCREMENT, /* NOT NULL means that this column can not be empty, and it is called a constraint */
-    item_id INT NOT NULL,
     product_name VARCHAR(255),
     department_name VARCHAR(255),
-    price NUMBER(10,4) NULL,
+    price INT NOT NULL,
     stock_quantity INT NOT NULL,
     PRIMARY KEY(id) /* if you don't do line 11, you get an error */
 );
@@ -25,20 +24,19 @@ CREATE TABLE products (
 
 /* inserts two rows*/
 /* do not insert into the id column*/
-INSERT INTO products (item_id, product_name, department_name, price, stock_quantity) 
+INSERT INTO products (product_name, department_name, price, stock_quantity) 
 VALUES 
-(1000, 'Cold Brew', 'Beverages', 4, 100),
-(1001, 'Drip Coffee', 'Beverages', 4, 100),
-(1002, 'New Orleans Iced Coffee', 'Beverages', 4, 60),
-(1003, 'Latte', 'Beverages', 5, 80),
-(1004, 'Mocha', 'Beverages', 5, 80),
-(1005, 'Vegan Almond Latte', 'Beverages', 5, 90),
-(1022, 'Tote Bag', 'Merchandise', 15, 50),
-(1023, 'Hydroflask', 'Merchandise', 20, 50),
-(1024, 'Coffee Sweater', 'Merchandise', 40, 30),
-(1025, 'Mug', 'Merchandise', 15, 20),
-(1026, 'Coffee Scarf', 'Merchandise', 25, 15);
-
+('Cold Brew', 'Beverages', 4, 100),
+('Drip Coffee', 'Beverages', 4, 100),
+('New Orleans Iced Coffee', 'Beverages', 4, 60),
+('Latte', 'Beverages', 5, 80),
+('Mocha', 'Beverages', 5, 80),
+('Vegan Almond Latte', 'Beverages', 5, 90),
+('Tote Bag', 'Merchandise', 15, 50),
+('Hydroflask', 'Merchandise', 20, 50),
+('Coffee Sweater', 'Merchandise', 40, 30),
+('Mug', 'Merchandise', 15, 20),
+('Coffee Scarf', 'Merchandise', 25, 15);
 
 SELECT * FROM products;
-
+DROP INDEX index ON products;
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,19 +1,101 @@
 var mysql = require('mysql');
 
+var inquirer = require("inquirer");
+
+
 // mysql -u root -p
 var connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
-  password : 'password',
-  database : 'bamazon_db',
-  port : 3306
+    host: 'localhost',
+    user: 'root',
+    password: 'password',
+    database: 'bamazon_db',
+    port: 3306
 });
- 
+
 connection.connect();
- 
-connection.query('SELECT * FROM products', function (error, results, fields) {
-  if (error) throw error;
-  console.table(results);
+//shows all products available in the store
+function allProducts(){
+connection.query("SELECT * FROM products", function (error, results, fields) {
+    if (error) throw error;
+    console.log('-----------')
+    console.log('-----------')
+    console.log('---allProducts()----')
+    console.table(results);
+    console.log('-----------')
+
+    purchase();
 });
- 
-connection.end();
+}
+allProducts();
+
+//setting a value to purchase and prompting user to enter ID and quantity of the product wanted
+var purchase=function(){
+    inquirer.prompt([{
+        name:'itemID',
+        type:'input',
+        message:'Enter item number:'
+    },{
+        name:'quantity',
+        type:'input',
+        message:'How many units of the product did you want to purchase?'
+    }]).then(function(resp){
+        idNumber = resp.itemID;
+        console.log('You chose item id:', idNumber);
+        quantitySelected = resp.quantity;
+        console.log('Quantity desired:', quantitySelected);
+        findProductBasedOnID(idNumber);
+
+
+
+//Created a function that would locate the product ID and console.logs the price x quantity wanted.
+        function findProductBasedOnID(pid){
+            connection.query('SELECT id, product_name, price, stock_quantity FROM products WHERE id=?', [pid], function(error, results, fields){
+                if (error) throw error;
+                    console.log('-----------------------------------')
+                    console.log(results)
+                    console.log('------------------------------------')
+                
+                var quantityRemaining = results[0].stock_quantity - parseInt(quantitySelected);
+//If we have more quantity than the user selected. 
+                if (quantityRemaining>quantitySelected){
+                    console.log('***********************************************************************************')
+                    console.log('You have selected ' + quantitySelected + ' order(s) of ' +results[0].product_name + ' at the price of $' + results[0].price + ' each.' )
+                    console.log('Quantity of Product Remaining: ' + quantityRemaining);
+                    console.log('Your total is : $ ' + results[0].price * parseInt(quantitySelected));
+                    console.log('***********************************************************************************')
+                    continuePurchasing();
+                    connection.query('UPDATE products SET stock_quantity = ? WHERE id=?',[quantityRemaining,pid])
+                }else{
+
+//else statement that returns if quantityRemaining is less than the quantity the user selected.
+                    console.log('***********************************************************************************')
+                    console.log('We don\'t have enough in stock! Check back later sorry!');
+                    console.log('***********************************************************************************')
+                    continuePurchasing();
+                }
+
+//function that would ask user if they want to continue purchasing or exit.
+                function continuePurchasing(){
+                    inquirer.prompt([{
+                        type:'list',
+                        message: 'Do you want to continue purchasing?',
+                        choices:['Continue','Exit'],
+                        name:'continue'
+                    }])
+                    .then(function(resp){
+                        switch (resp.continue){
+                            case 'Continue':
+                                purchase();
+                                break;
+                            case 'Exit':
+                                console.log('Thank you! See ya next time!')
+                                connection.end();
+                                break;
+                        }
+                    })
+                }
+            })
+        }
+        
+    })
+}
